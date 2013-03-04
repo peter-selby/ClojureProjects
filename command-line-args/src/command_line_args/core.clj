9792983408c61fea6f41d2af08053cd7a3e82d27
Brian Beckman <bc.beckman@gmail.com>
2013-03-03 16:11:34 -0800

    learning progress

--- command-line-args/src/command_line_args/core.clj
+++ command-line-args/src/command_line_args/core.clj
@@ -5,4 +5,41 @@
                   [(keyword (.replace k "--" "")) v])
                 (partition 2 args))))
 
+(defn keywordize [kvp]
+  (let [[k v] kvp]
+    [(keyword (.replace k "--" ""))
+     v]))
 
+;(println "boo")
+;(println (keywordize ["a" 1 "b" 2]))
+
+(def ^:dynamic v 1) ; v is global and dynamic
+
+(defn f1 []
+  (println "f1: v: " v))
+
+(defn f2 []
+  (println "f2: before let v: " v)
+  (let [v 2]
+    (println "f2: in let, v: " v)
+    (f1))
+  (println "f2: after let v: " v))
+
+(defn f3 []
+  (println "f3: before binding v: " v)
+  (binding [v 3]
+    (println "f3: in binding v: " v)
+    (f1))
+  (println "f3: after binding v: " v))
+
+(defn f4 []
+  (def v 4))
+
+(println "(= v 1) ~~> " (= v 1))
+(f2)
+(f3)
+(f4)
+(println "(= v 4) ~~> " (= v 4))
+
+(defn power [base & exponents]
+  (reduce #(Math/pow %1 %2) base exponents))
--- command-line-args/test/command_line_args/core_test.clj
+++ command-line-args/test/command_line_args/core_test.clj
@@ -13,4 +13,64 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 1 1))))
+    (is (= 1
+           1))))
+
+(deftest b-test
+  (testing "keywordize [\"a\"] = [:a nil] -- missing value defaults to nil"
+    (is (= (keywordize ["a"])
+           [:a nil]))))
+
+(deftest c-001-test
+  (testing "(keywordize [\"a\" 1 \"b\" 2]) -- extra values ignored"
+    (is (= (keywordize ["a" 1 "b" 2])
+           [:a 1]))))
+
+(deftest c-002-test
+  (testing "(keywordize [\"a\" 3 2]) -- extra values ignored"
+    (is (= (keywordize ["a" 3 2])
+           [:a 3]))))
+
+(deftest d-test
+  (testing "dynamic binding and let shadowing"
+    (is (= v
+           4))))
+
+(deftest e-001-test
+  (testing "count on vectors"
+    (is (count [19 "yellow" true])
+        3)))
+
+(deftest e-002-test
+  (testing "reverse a vector"
+    (is (= (reverse [2 4 7])
+           [7 4 2]))))
+
+(deftest e-003-test
+  (testing "map over multiple collections of differing lengths"
+    (is (= (map +
+                [2 4 7]
+                [5 6]
+                [1 2 3 4])
+           [8 12]))))
+
+(defn parting
+  "returns a String containing a parting salutation, an anti-greeting
+  if you will, and I am making this documentation extra long so that I
+  can test C-c M-q for formatting the documentation."
+  ; we don't have an attr-map
+  [name] ; these are the params
+  ; we don't have a prepost-map
+  (str "Goodbye, " name)
+  )
+
+(deftest f-test
+  (testing "test the parting function"
+    (is (= (parting "Mark")
+           "Goodbye, Mark"))))
+
+(deftest g-test
+  (testing "testing multiary power function"
+    (is (= (power 2 3 4)
+           4096.0)))) ; definitely NOT 4096 the integer
+
