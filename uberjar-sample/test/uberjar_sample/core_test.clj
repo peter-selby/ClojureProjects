885f6877cd94b0e496203b2e7160568e3762b037
Brian Beckman <bc.beckman@gmail.com>
2013-03-04 18:01:47 -0800

    finishing up uberjars sample

--- uberjar-sample/test/uberjar_sample/core_test.clj
+++ uberjar-sample/test/uberjar_sample/core_test.clj
@@ -2,6 +2,18 @@
   (:use clojure.test
         uberjar-sample.core))
 
+(defn a-fixture [test-fn]
+  (let [v 1] (assert (= v 1)))
+  (test-fn)
+  (let [v 2] (assert (= v 2))))
+
+(use-fixtures :each a-fixture)
+
 (deftest a-test
   (testing "I'm FIXED."
     (is (= 1 1))))
+
+(deftest b-test
+  (testing "I'm ok too"
+    (is (= 2 2))))
+
