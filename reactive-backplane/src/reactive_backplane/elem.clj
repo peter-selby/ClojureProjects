adf8196e8705dadb670e0501e983e85c8a2aaebe
Brian Beckman <bc.beckman@gmail.com>
2013-03-16 17:39:04 -0700

    all tests passing

--- reactive-backplane/src/reactive_backplane/elem.clj
+++ reactive-backplane/src/reactive_backplane/elem.clj
@@ -1,14 +1,43 @@
 (ns reactive-backplane.elem
   (:refer-clojure :exclude (list get delete)))
 
-(def elems (atom {}))
-(defn list [] @elems)
-(defn get [id] (@elems id))
-(defn put [id attrs]
+(def elems
+  "Public singleton atom containing the time-varying state of the
+web-service in the form of all key-value pairs that have been PUT."
+  (atom {}))
+
+(defn list
+  "Dereference elems, producing a clojure map of all key-value pairs
+  that have been PUT to the seb service up to the time of this call."
+  []
+  @elems)
+
+(defn get
+  "Get the value corresponding to the id (key) from the web service,
+or nil if the key is not present."
+  [id]
+  (@elems id))
+
+(defn put
+  "Put the key-value pair \"id\" \"attrs\" into the web-service
+  state."
+  [id attrs]
   (let [new-attrs (merge (get id) attrs)]
     (swap! elems assoc id new-attrs)
     new-attrs))
-(defn delete [id]
+
+(defn delete
+  [id]
+  "Delete the key \"id\" and its corresponding value from the
+  web-service state."
   (let [old-attrs (get id)]
     (swap! elems dissoc id)
     old-attrs))
+
+(defn clear
+  []
+  "Clear the entire state of the web-service."
+  (reset! elems {})                     ; alternative:
+                                        ; (compare-and-set! elems
+                                        ; @elems (atom {})
+  )
--- reactive-backplane/test/reactive_backplane/test/elem.clj
+++ reactive-backplane/test/reactive_backplane/test/elem.clj
@@ -3,9 +3,34 @@
   (:use clojure.test
         [reactive-backplane.elem :as e]))
 
-(deftest test-elem
-  (testing "elem operations"
-    (is (= (e/list) {}))
-    (is (= (e/put 42 [:a 7])))
-    (is (= (e/list) {42 {:a 7}}))
+;;; see http://richhickey.github.com/clojure/clojure.test-api.html
+
+(is true "Test presence of the test framework first.")
+
+(deftest test-elem-normal-operations
+  (testing "Sequential test of service state as a stateful atom, normal operations."
+    (is (= (e/clear           ) {}         ) "Clear the service state.")
+    (is (= (e/list            ) {}         ) "Show the service state.")
+    (is (= (e/get    42       ) nil        ) "Test missing key.")
+    (is (= (e/put    42 [:a 7]) {:a 7}     ) "Key-value vectors converted to maps.")
+    (is (= (e/list            ) {42 {:a 7}}) "State with nested maps.")
+    (is (= (e/get    42       ) {:a 7}     ) "Value for a key.")
+    (is (= (e/get    37       ) nil        ) "Test another missing key.")
+    (is (= (e/put    37 [:b 8]) {:b 8}     ) "When putting, produce only the value put.")
+    (is (= (e/list            ) {37 {:b 8}, 42 {:a 7}})) "Show entire service state as a map."
+    (is (= (e/delete 42       ) {:a 7}     ) "Delete produces the value deleted.") 
+    (is (= (e/get    42       ) nil        ) "Key should be missing after delete.")
+    (is (= (e/delete 42       ) nil        ) "Deleting a missing key produces nil.")     
+    (is (= (e/list            ) {37 {:b 8}}) "Remaining keys and values left by delete.") 
+    (is (= (e/clear           ) {}         ) "Clear produces empty map.")
+    (is (= (e/delete 42       ) nil        ) "Deleting from empty map is harmless.")     
+    (is (= (e/list            ) {}         ) "Empty map should show as empty.")
     ))
+
+(deftest test-elem-abnormal-operations
+  (testing "Sequential test of stateful elem atom, abnormal operations."
+    (is (thrown? clojure.lang.ArityException (e/put)))
+    (is (thrown? clojure.lang.ArityException (e/put 42)))
+    (is (thrown? clojure.lang.ArityException (e/list 42)))
+    ))
+
