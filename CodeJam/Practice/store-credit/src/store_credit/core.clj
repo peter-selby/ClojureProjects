01dd0ef3f120741902926bd5b38daa5979b02cfb
Brian Beckman <bc.beckman@gmail.com>
2013-03-30 09:38:28 -0700

    algo prep

--- CodeJam/Practice/store-credit/src/store_credit/core.clj
+++ CodeJam/Practice/store-credit/src/store_credit/core.clj
@@ -1,7 +1,10 @@
 (ns store-credit.core
   (:use [clojure.java.io :as cjio])
-  (:use [clojure.string  :as cstr])
+  (:use [clojure.string  :as cstr :only [split split-lines]])
+  (:use [clojure.pprint  :as pp   :only [pprint]])
   (:gen-class))
+
+;;; This is speed-code: very very light on error-checking and testing.
 
 (defn get-current-directory []
   (. (java.io.File. ".") getCanonicalPath))
@@ -10,6 +13,12 @@
   (with-open [rdr (cjio/reader "input.txt")]
     (doseq [line (line-seq rdr)]
       (f line))))
+
+(defn case-from-lines [triple]
+  {:credit (read-string (first triple))
+   :nitems (read-string (nth triple 1))
+   :prices (map read-string (cstr/split (nth triple 2) #"\s"))}
+  )
 
 (defn -main
   "Solve codeJam practice problem https://code.google.com/codejam/contest/351101/dashboard#s=p0."
@@ -20,11 +29,23 @@
   (println (get-current-directory))
 
   (let [input (slurp "input.txt")
-        lines (cstr/split-lines input)]
-    
-    lines)
+        lines (cstr/split-lines input)
+        n (read-string (first lines))
+        flat-cases (rest lines)
+        cases (reduce
+               (fn [cases triple] (conj cases (case-from-lines triple)))
+               []
+               (partition 3 flat-cases))]
 
+    (pp/pprint cases)
 
+    cases
+
+    #_(map
+     (fn [line] (println line))
+     cases)
+
+    )
 
   #_(do-per-line println)
 
