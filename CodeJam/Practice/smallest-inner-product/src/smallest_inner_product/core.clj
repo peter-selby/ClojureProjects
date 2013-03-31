df13ba6491b3ec295ff99ea7fb7baed8f2f689b9
Brian Beckman <bc.beckman@gmail.com>
2013-03-31 10:46:29 -0700

    smallest-inner-product practice solved

--- CodeJam/Practice/smallest-inner-product/src/smallest_inner_product/core.clj
+++ CodeJam/Practice/smallest-inner-product/src/smallest_inner_product/core.clj
@@ -21,24 +21,42 @@
          (println "----------------")
          x#))) 
 
+(defn inner [v1 v2] (apply + (map * v1 v2)))
+
+(defn smallest-inner [v1 v2] (inner (sort > v1) (sort < v2)))
+
+(defn str-of-components->vec [s]
+  (let [strs (cstr/split s #"\s")
+        nums (map read-string strs)]
+    (vec nums))
+  )
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
   (let [input (slurp
-               "/Users/rebcabin/Downloads/C-large-practice.in"
+               "/Users/rebcabin/Downloads/A-large-practice (2).in"
                ;"input.txt"
                )
         _     (spit "input.txt" input)
         lines (cstr/split-lines input)
+        ;; The (-> l vec) incantation just forces the lazy partition.
+        parts (map (fn [l] (-> l vec)) (partition 3 (rest lines)))
+        vects (map (fn [part]
+                     {:dim (part 0),
+                      :v1 (str-of-components->vec (part 1)),
+                      :v2 (str-of-components->vec (part 2))
+                      })
+                   parts)
         answs (map-indexed
                (fn [i l]
                  (str "Case #" (inc i) ": "
-                      (identity l)
+                      (smallest-inner (:v1 l) (:v2 l))
                       "\n")
                  )
-               (rest lines)
+               vects
                )
         ]
     (with-open [w (cjio/writer "output.txt")]
