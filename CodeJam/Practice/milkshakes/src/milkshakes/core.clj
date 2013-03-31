628af7cfade6792b158fc2e43b7285c5d8a70b80
Brian Beckman <bc.beckman@gmail.com>
2013-03-31 14:38:42 -0700

    prep milkshakes

--- CodeJam/Practice/milkshakes/src/milkshakes/core.clj
+++ CodeJam/Practice/milkshakes/src/milkshakes/core.clj
@@ -1,9 +1,17 @@
-(ns smallest-inner-product.core
+(ns milkshakes.core
   (:require clojure.set)
   (:use [clojure.java.io :as cjio])
   (:use [clojure.string  :as cstr :only [split split-lines]])
   (:use [clojure.pprint  :as pp   :only [pprint]])
   (:gen-class))
+
+(defn get-current-directory []
+  (. (java.io.File. ".") getCanonicalPath))
+
+(defn do-per-line [f]
+  (with-open [rdr (cjio/reader "input.txt")]
+    (doseq [line (line-seq rdr)]
+      (f line))))
 
 (defmacro dbg [x]
   `(let [x# ~x]
@@ -19,46 +27,46 @@
          (println "~~>")
          (clojure.pprint/pprint x#)
          (println "----------------")
-         x#))) 
+         x#)))
 
-(defn inner [v1 v2] (apply + (map * v1 v2)))
+(defn parse-cases [n ls]
 
-(defn smallest-inner [v1 v2] (inner (sort > v1) (sort < v2)))
-
-(defn str-of-components->vec [s]
-  (let [strs (cstr/split s #"\s")
-        nums (map read-string strs)]
-    (vec nums))
   )
 
+(defn parse-lines [ls]
+  (let [ncases (read-string (first ls))
+        acases (parse-cases ncases (rest ls))]
+    acases
+  ))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Basic husk for programming problems."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
+  (println (get-current-directory))
+  ;; (let [input (slurp "input.txt")]
+  ;;   (println input))
+  ;; (do-per-line println)
+  ;; (with-open [wrtr (cjio/writer "output.txt")]
+  ;;   (do-per-line (fn [l] (.write wrtr l))))
+  ;; (slurp "output.txt")
   (let [input (slurp
-               ;"/Users/rebcabin/Downloads/A-large-practice (2).in"
+               ;"/Users/rebcabin/Downloads/C-large-practice.in"
                "input.txt"
                )
         _     (spit "input.txt" input)
         lines (cstr/split-lines input)
-        ;; The (-> l vec) incantation just forces the lazy partition.
-        parts (map (fn [l] (-> l vec)) (partition 3 (rest lines)))
-        vects (map (fn [part]
-                     {:dim (part 0),
-                      :v1 (str-of-components->vec (part 1)),
-                      :v2 (str-of-components->vec (part 2))
-                      })
-                   parts)
         answs (map-indexed
                (fn [i l]
                  (str "Case #" (inc i) ": "
-                      (smallest-inner (:v1 l) (:v2 l))
+                      (identity l)
                       "\n")
                  )
-               vects
+               (rest lines)
                )
         ]
     (with-open [w (cjio/writer "output.txt")]
       (doseq [line answs] (.write w line)))
     ))
+
