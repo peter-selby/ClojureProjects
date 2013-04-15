40c6354548aaa167971b2889a6c804398bd88d7b
Brian Beckman <bc.beckman@gmail.com>
2013-04-15 08:39:51 -0700

    basic web sockets up and running

--- ws-intro/project.clj
+++ ws-intro/project.clj
@@ -1,4 +1,5 @@
 (defproject ws-intro "0.1.0-SNAPSHOT"
+  :main ws-intro.core
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- ws-intro/src/ws_intro/core.clj
+++ ws-intro/src/ws_intro/core.clj
@@ -4,6 +4,17 @@
   (:import  [org.webbitserver WebServer WebServers WebSocketHandler]
             [org.webbitserver.handler StaticFileHandler]))
 
+(defn -main []
+  "Thanks to blog.jayfields.com"
+  (doto (WebServers/createWebServer 8080)
+    (.add "/websocket"
+          (proxy [WebSocketHandler] []
+            (onOpen    [c  ] (println "websocket opened" c))
+            (onClose   [c  ] (println "websocket closed" c))
+            (onMessage [c j] (println "websocket message" c j))))
+    (.add (StaticFileHandler. "."))
+    (.start)))
+
 (defn foo
   "I don't do a whole lot."
   [x]
