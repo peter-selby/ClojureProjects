63405081dda7ead51a4023ab25a5dbf1c7d0ea2b
Brian Beckman <bc.beckman@gmail.com>
2013-04-17 21:37:32 -0700

    beginnings of fixed-length buffered aggregates

--- ws-intro/src/ws_intro/core.clj
+++ ws-intro/src/ws_intro/core.clj
@@ -1,7 +1,9 @@
 (ns ws-intro.core
-  (:require [clojure.data.json :as json]
-            [clojure.string    :as s]
-            [clojure.pprint    :as pp])
+  (:require [clojure.data.json        :as json]
+            [clojure.string           :as s   ]
+            [clojure.pprint           :as pp  ]
+            [ws-intro.ring-buffer     :as rb  ]
+            )
   (:import  [org.webbitserver         WebServer WebServers WebSocketHandler]
             [org.webbitserver.handler StaticFileHandler                    ]
             [rx                       Observable Observer Subscription     ]
@@ -24,14 +26,16 @@
 ;;; internally send messages to the client via websockets.
 
 (def ^:private domains
-  ["vendorinfoportal.amazon.com", 
-   "w.amazon.com",                
-   "cr.amazon.com",               
-   "external",                    
-   "vendormaster.amazon.com",     
-   "permissions.amazon.com",      
-   "devcentral.amazon.com",       
-   "build.amazon.com",])
+  [
+   "vendorinfoportal.amazon.com"
+   "w.amazon.com"
+   "cr.amazon.com"
+   "external"
+   "vendormaster.amazon.com"
+   "permissions.amazon.com"
+   "devcentral.amazon.com"
+   "build.amazon.com"
+   ])
 
 (defn cheapGaussian [mean sigma]
   (let [sqrt12 3.46410161514
@@ -49,23 +53,27 @@
                       1000.0)))
 
 (defn- randomizedDomainHits []
-  (map (fn [domain] {"domain" domain, "hitsPerSec" (randrate)}) domains)
+  (map (fn [domain]
+         {"domain"     domain,
+          "hitsPerSec" (randrate),
+          "url"        domain,
+          }) domains)
   )
 
 (def ^:private fromTos
   [{:from ["vendorinfoportal.amazon.com"], :tos ["vendorinfoportal.amazon.com"
-                                               "w.amazon.com"
-                                               "external"
-                                               "vendormaster.amazon.com"]}
+                                                 "w.amazon.com"
+                                                 "external"
+                                                 "vendormaster.amazon.com"]}
    {:from ["w.amazon.com"], :tos ["w.amazon.com"
-                                "external"
-                                "vendorinfoportal.amazon.com"]}
+                                  "external"
+                                  "vendorinfoportal.amazon.com"]}
    {:from ["vendormaster.amazon.com"], :tos ["vendormaster.amazon.com"
-                                           "w.amazon.com"
-                                           "external"
-                                           "permissions.amazon.com"
-                                           "devcentral.amazon.com"
-                                           ]}]
+                                             "w.amazon.com"
+                                             "external"
+                                             "permissions.amazon.com"
+                                             "devcentral.amazon.com"
+                                             ]}]
   )
 
 (defn- randomizedReferralMatrix []
@@ -108,9 +116,29 @@
                   :message (s/upper-case msg)}))
     ))
 
+(def bufferLength         50)
+(def domainCounts         (atom {}))
+(def bufferedDomainCounts (atom (rb/ring-buffer bufferLength)))
+(def test                 (atom 0))
+
+(defn beef-domain-counts [urls]
+  (doseq [url urls]
+    (println url)
+    (let [c (@domainCounts url)]
+      (swap! domainCounts #(into % {url (inc (or c 0))}))
+      )
+    ))
+
 (defn -main []
-  "Thanks to blog.jayfields.com"
-  (pp/pprint (getMock))
+  "Thanks to blog.jayfields.com and amalloy/ring-buffer AT github"
+  ;; Just a quick unit test to remind us how to call the ring buffer.
+  (pp/pprint (= [1 1 1 1]
+                (let [b (rb/ring-buffer 3)]
+                  [(if (= '(a b  ) (into b '(a b)))             1)
+                   (if (= '(c d e) (into b '(a b c d e)))       1)
+                   (if (= '(d e  ) (pop (into b '(a b c d e)))) 1)
+                   (if (= 'c (peek (into b '(a b c d e))))      1)
+                   ])))
   (let [server (WebServers/createWebServer 8080)]
     (doto server
       (.add "/websocket"
@@ -119,28 +147,31 @@
               (onOpen    [conn]
                 (println "WEBSOCKET OPENED"  conn)
                 (-> (mockObservable (getMock))
+                    (.map (fn [item]
+                            (let [urls
+                                  (map #(% "url")
+                                       (get-in item ["hitRateAggs"]))]
+                              (beef-domain-counts urls)
+                              #_[(swap! test inc) urls]
+                              #_domainCounts
+                              )))
                     (.subscribe
                      (fn [datum]
                        (-> conn
                            (.send (json/write-str
-                                    {:type "hitdata"
-                                     :message (str datum)}))))
-                     ))
-                )
+                                   {:type "hitdata"
+                                    :message (str (vec datum))}))))
+                     )))
 
               (onClose   [c  ]
                 (println "WEBSOCKET CLOSED"  c)
                 )
 
               (onMessage [c j]
-                ;(println "WEBSOCKET MESSAGE" c j)
+                                        ;(println "WEBSOCKET MESSAGE" c j)
                 (on-message c j)
                 )
               ))
       (.add (StaticFileHandler. "."))
       (.start))))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
