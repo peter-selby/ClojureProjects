5f9fc87de70b603c08100f4a5e4867b998b99dd4
Brian Beckman <bc.beckman@gmail.com>
2013-06-16 08:33:49 -0700

    update palindromics

--- Exercises/palindromics/src/palindromics/core.clj
+++ Exercises/palindromics/src/palindromics/core.clj
@@ -1,43 +1,43 @@
-(ns palindromics.core)
+(ns palindromics.core
+  (:require [clojure.math.numeric-tower :as math]))
+
+(defmacro dump [x]
+  `(let [x# ~x]
+     (do (println '~x "~~> ")
+       (clojure.pprint/pprint x#)
+       (println "")
+       x#)))
 
 (def i 1881)
+(def is [2 9 11 88 99 181 191 999 1881 1991 9999 18281 18981 19991 99999 189981])
 (odd? 42)
 
-(letfn [(gary   [n]  (cons n (lazy-seq (gary (*' 10 n)))))
-        (fran   [ds] (apply +' (map *' ds (gary 1))))
-        (heads  [n]  (take-while pos? (map (partial quot n) (gary 1))))
-        (digits [n]  (map #(mod % 10) (heads n)))
-        (next   [n]  (let [rigits (reverse (digits n))
-                           kount  (count rigits)
-                           k2     (quot kount 2)
-                           fron   (take k2 rigits)
-                           lfron  (last fron)
-                           nlfron (when lfron (inc lfron))
-                           bfron  (butlast fron)
-                           oscar  (odd? kount)
-                           mid    (when oscar (nth rigits k2))
-                           nmid   (when mid (inc mid))
-                           ]
-                       (if oscar
-                         ;; odd-length
-                         (if nmid   1 0)
-                         ;; even-length
-                         (if nlfron 
-                           (if (< nlfron 9)
-                             (let [bob (concat bfron [nlfron])]
-                               (fran (concat bob (reverse bob))))
-                             (let [carol (inc (fran fron))
-                                   dave  (digits carol)]
-                               (fran (concat (reverse dave) dave))
-                               ))
-                           (throw (Exception.) "impossible")
-                           )
-                         )
-                       )
-          )
-        ]
-  (next i)
-  )
+(def edward
+  (fn [i]
+    (letfn [(gary   [n]  (cons n (lazy-seq (gary (*' 10 n)))))
+            (heidi  [n]  (take-while pos? (map (partial quot n) (gary 1))))
+            (dave   [n]  (map #(mod % 10) (heidi n)))
+            (karen  [n]  (count (dave n)))
+            (oscar  [n]  (odd? (karen n)))
+            (half   [n]  (let [ds (dave n), k (count ds), h (quot k 2), j
+                               (if (odd? k) (inc h) h)]
+                           (take j ds)))
+            (fran   [ds] (apply +' (map *' (reverse ds) (gary 1))))
+            (don    [n]  (dave (inc (fran (half n)))))
+            (kathy  [n]  (count (don n)))
+            ]
+      (let [odd (oscar i), bump (> (kathy i) (count (half i)))]
+        (fran
+         (let [d (don i)]
+           (if bump
+             (let [r (reverse d)]
+               (concat (if odd (butlast r) r) (rest d)))
+             (concat (reverse d) (if odd (rest d) d)))))))))
+
+(doseq [i is] (println (edward i)))
+(println "")
+
+
 
 
 
