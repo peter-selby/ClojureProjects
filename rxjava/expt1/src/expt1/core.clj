e1ebcd7be2ca7d7ba895a0fb104ee48e6ed84ae2
Brian Beckman <bc.beckman@gmail.com>
2013-06-21 12:53:31 -0700

    through exercise 25

--- rxjava/expt1/src/expt1/core.clj
+++ rxjava/expt1/src/expt1/core.clj
@@ -863,3 +863,151 @@
 ;;;   )
 ;;; )
 
+;;;    ____                 _           ___  ____
+;;;   / __/_ _____ ________(_)__ ___   |_  |/ __/
+;;;  / _/ \ \ / -_) __/ __/ (_-</ -_) / __//__ \ 
+;;; /___//_\_\\__/_/  \__/_/___/\__/ /____/____/ 
+                                             
+;;; Exercise 25: Converting from Arrays to Trees
+
+;;; We have 2 arrays each containing lists, and videos respectively. Each
+;;; video has a listId field indicating its parent list. We want to build an
+;;; array of list objects, each with a name and a videos array. The videos
+;;; array will contain the video's id and title.
+
+;;; Input
+
+;;; lists:
+;;;         [
+;;;             {
+;;;                 "id": 5434364,
+;;;                 "name": "New Releases"
+;;;             },
+;;;             {
+;;;                 "id": 65456475,
+;;;                 name: "Thrillers"
+;;;             }
+;;;         ]
+;;;  
+;;; videos:
+;;;         [
+;;;             {
+;;;                 "listId": 5434364,
+;;;                 "id": 65432445,
+;;;                 "title": "The Chamber"
+;;;             },
+;;;             {
+;;;                 "listId": 5434364,            
+;;;                 "id": 675465,
+;;;                 "title": "Fracture"
+;;;             },
+;;;             {
+;;;                 "listId": 65456475,            
+;;;                 "id": 70111470,
+;;;                 "title": "Die Hard"
+;;;             },
+;;;             {
+;;;                 "listId": 65456475,                        
+;;;                 "id": 654356453,
+;;;                 "title": "Bad Boys"
+;;;             }
+;;;         ]
+;;; Output
+;;; 
+;;; [
+;;;     {
+;;;         "name": "New Releases",
+;;;         "videos": [
+;;;             {
+;;;                 "id": 65432445,
+;;;                 "title": "The Chamber"
+;;;             },
+;;;             {
+;;;                 "id": 675465,
+;;;                 "title": "Fracture"
+;;;             }
+;;;         ]
+;;;     },
+;;;     {
+;;;         "name": "Thrillers",
+;;;         "videos": [
+;;;             {
+;;;                 "id": 70111470,
+;;;                 "title": "Die Hard"
+;;;             },
+;;;             {
+;;;                 "id": 654356453,
+;;;                 "title": "Bad Boys"
+;;;             }
+;;;         ]
+;;;     }
+;;; ]
+
+;;; Javascript
+;;; 
+;;; return lists.map(
+;;;   function (list) {
+;;;     return {
+;;;       name: list.name,
+;;;       videos: videos
+;;;         .filter(
+;;;           function (video) {
+;;;             return video.listId === list.id;
+;;;           })
+;;;         .map(
+;;;           function (video) {
+;;;             return {
+;;;               id: video.id,
+;;;               title: video.title
+;;;             };
+;;;           })
+;;;     };
+;;;   });
+
+;;; Datapath
+;;; 
+;;; (exist (list)
+;;;   (and
+;;;     (.* lists list)
+;;;     (= result {
+;;;         name: (. list "name"),
+;;;         videos: (list (v)
+;;;           (exist (video)
+;;;             (and
+;;;               (.* videos video)
+;;;               (. video "listId" (. list "id"))
+;;;               (= v {
+;;;                   id: (. video "id"),
+;;;                   title: (. video "title")
+;;;                 })
+;;;             )
+;;;           ))
+;;;       })
+;;;   )
+;;; )
+
+(let [lists  (-> (jslurp "Exercise_25_lists.json")  asynchronous-observable)
+      videos (-> (jslurp "Exercise_25_videos.json") asynchronous-observable)]
+
+  (-> lists
+      (.map (fn [lyst]
+              {:name (lyst "name")
+               :videos
+               (-> videos
+                   (.filter (fn [vid] (== (vid "listId") (lyst "id"))))
+                   (.map (fn [vid] {:id (vid "id") :title (vid "title")}))
+                   )
+               }
+              ))
+
+      subscribe-collectors
+      pdump
+      :onNext
+      ((flip map)
+       (fn [lyst]
+         {:name (lyst :name)
+          :videos (-> (lyst :videos)
+                      subscribe-collectors
+                      :onNext)}
+         )))
+  )
