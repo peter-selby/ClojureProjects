c87522846311466a728b069d72dbed6bce9feee2
rebcabin <bc.beckman@gmail.com>
2013-06-30 07:04:35 -0700

    Support clojuredocs client in repl

--- rxjava/expt1/project.clj
+++ rxjava/expt1/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure               "1.5.1"]
                  [clj-http                          "0.7.1"]
+                 [org.thnetos/cd-client             "0.3.6"]
                  ;; [org.slf4j/log4j-over-slf4j        "1.6.6"]
                  [org.slf4j/slf4j-log4j12           "1.7.5"]
                  [enlive                            "1.1.1"]
