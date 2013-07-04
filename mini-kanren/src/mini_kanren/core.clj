1bdd59815f3d989a32771cb5eff03c7798895023
rebcabin <bc.beckman@gmail.com>
2013-07-04 11:50:09 -0700

    a few more tests

--- mini-kanren/src/mini_kanren/core.clj
+++ mini-kanren/src/mini_kanren/core.clj
@@ -24,7 +24,7 @@
 (test/deftest foo-test
   (test/is (= '(true)  (run* [q] (== q true))))
   (test/is (= '(true)  (run* [q] s# (== true q))))
-  (test/is (= '()      (run* [q] u# (== true (pdump q)))))
+  (test/is (= '()      (run* [q] u# (== true q))))
   (test/is (= '(corn)  (run* [q] s# (== 'corn q))))
   (test/is (= '(false) (run* [q] s# (== false q))))
   (test/is (= '()      (run* [q] (== false 'x))))
@@ -32,5 +32,14 @@
   (test/is (= '(_0)    (run* [x] (let [x true] (== true x)))))
   (test/is (= '(true)  (run* [q] (fresh [x] (== true x) (== true q)))))
   (test/is (= '(true)  (run* [q] (fresh [x] (== false x) (== true q)))))
+  (test/is (= '(_0)    (run* [x] s#)))
+  (test/is (= '(_0)    (run* [x])))
+  (test/is (= '(_0)    (run* [x] (let [x false] (fresh [x] (== true x))))))
+  (test/is (= '((_0 _1))
+              (run* [r] (fresh [x y] (== (lcons x (lcons y ())) r)))))
+  (test/is (= '((_0 _1 _0))
+              (run* [r] (fresh [x y] (== (lcons x (lcons y (lcons x ()))) r)))))
+  (test/is (= '((_0 _1 _0))
+              (run* [r] (fresh [x y] (== (lcons y (lcons x (lcons y ()))) r)))))
   )
 
