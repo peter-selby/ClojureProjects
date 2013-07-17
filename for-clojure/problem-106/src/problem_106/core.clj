98b251fcfa9abdaf0bb1eda85fc300a5388e90e0
Brian Beckman <bc.beckman@gmail.com>
2013-07-17 11:32:15 -0700

    big improvement on 106 (ideas from youz's solution)

--- for-clojure/problem-106/src/problem_106/core.clj
+++ for-clojure/problem-106/src/problem_106/core.clj
@@ -24,6 +24,22 @@
                       (even? n) (min dh ah hh)))))] 
           (fn [s t] (hop-count s t 10 1))))
 
+(def __ #((fn r [n s]
+            (if ((set s) %2) n
+                (r (+ n 1)
+                   (for [f [+ * /] e s] (f e 2)))))
+          1 [%]))
+
+(def __ (letfn [(d [n] (* n 2)) ; double
+                (h [n] (/ n 2)) ; halve
+                (a [n] (+ n 2)) ; add
+                (hop-count [n ts hops]
+                  (cond
+                   (ts n)      hops ; end search
+                   :else
+                   (hop-count n (set (concat ts (map d ts) (map a ts) (map h ts))) (inc hops))))] 
+          (fn [s t] (hop-count t #{s} 1))))
+
 (println (map #(apply __ %) [[1 1] [3 12] [12 3] [5 9] [9 2] [9 12]]))
 
 (defn -main [& args]
